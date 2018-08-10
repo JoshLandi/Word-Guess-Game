4d696a23450f9f57b393aab8724a0dbd8ed32abb
Josh L <joshtlandi@gmail.com>
2018-08-10 12:42:56 -0700

    added const

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,8 @@
 // Potential words to guess.
 var wordOptions = ["Buffalo", "Hot Sauce", "Juicy", "Mango Habenaro", "Delicious" ]
 
+// Player's max amount of guesses
+const MaxTries = 10
 
 // Logging player letter entry.
 var letterEntry = document.getElementById("letterEntry")
